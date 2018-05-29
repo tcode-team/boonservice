f05b26364c7f34fb08d66da660249b1b98630702
WorAun <worawut_aun@hotmail.com>
2018-05-29 13:04:15 +0700

    Add ShipmentList

---
+++ boonservice/Controllers/ShipmentListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace boonservice.Controllers
+{
+    public class ShipmentListController : Controller
+    {
+        // GET: ShipmentList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
