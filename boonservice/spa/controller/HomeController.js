69777d7fb9bfb6bbd42f1a3f7fdb351ecf7b86f5
Chalutpun <chalutpun@tcodev.com>
2018-05-29 13:06:14 +0700

    Add function menu

--- boonservice/spa/controller/HomeController.js
+++ boonservice/spa/controller/HomeController.js
@@ -4,15 +4,23 @@
     angular
         .module('app')
         .controller('HomeController', HomeController)
+        .filter('trustAsResourceUrl', ['$sce', function ($sce) {
+            return function (val) {
+                var url = $sce.trustAsResourceUrl(val);
+                return url;
+            };
+        }])
 
-    HomeController.$inject = ['$scope', '$http', '$rootScope', '$location', 'Idle'];
-    function HomeController($scope, $http, $rootScope, $location, Idle) {        
+    HomeController.$inject = ['$scope', '$http', '$rootScope', '$location'];
+    function HomeController($scope, $http, $rootScope, $location) {        
         $scope.init = function () {
 
         };
 
         $scope.UrlChange = function (url) {
-            
+            //url += 'userid=' + $scope.user.userid + '&username=' + $scope.user.username + '&position=' + $scope.user.position;
+            //url += '&vendor_code=' + $scope.user.vendor_code + '&first_name=' + $scope.user.first_name + '&last_name=' + $scope.user.last_name;
+            $scope.prgurl = url;
         };
 
         $scope.logout = function () {
